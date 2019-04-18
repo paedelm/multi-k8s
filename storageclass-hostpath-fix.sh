bea51e149b8e360456d888a03779ab5ed0e600c9
Peter <paedelm@gmail.com>
2019-04-18 10:14:12 +0200

    k8s init commit

---
+++ storageclass-hostpath-fix.sh
@@ -0,0 +1,20 @@
+kubectl delete storageclass hostpath
+
+rem Setting up hostpath provisioner.
+kubectl create -f https://raw.githubusercontent.com/MaZderMind/hostpath-provisioner/master/manifests/rbac.yaml
+kubectl create -f https://raw.githubusercontent.com/MaZderMind/hostpath-provisioner/master/manifests/deployment.yaml
+kubectl create -f https://raw.githubusercontent.com/MaZderMind/hostpath-provisioner/master/manifests/storageclass.yaml
+
+rem make ingress controller visible on localhost
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/mandatory.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/cloud-generic.yaml
+
+POD_NAME=$(kubectl get pods -l app.kubernetes.io/name=ingress-nginx -o jsonpath='{.items[0].metadata.name}')
+kubectl exec -it $POD_NAME -- /nginx-ingress-controller --version
+
+helm install stable/nginx-ingress --name my-nginx
+
+    kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/master/deploy/provider/baremetal/service-nodeport.yaml
+
+kubectl get pods --all-namespaces -l app.kubernetes.io/name=ingress-nginx --watch
